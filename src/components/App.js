cf6153e6914df2acaf6f801029113e8c80b54c8a
stark <anishgupta0066@gmail.com>
2021-11-11 12:12:47 +0530

    added VideoDetail component,did prop drilling and again a bit of styling,also added componentDidMount() method to handle blank page when the app loads

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
-import VideoList from './videoList';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
-    state = {videos : []};
+    state = {videos : [], selectedVideo : null};
+
+    componentDidMount(){
+        this.onTermSubmit('buildings');
+    }
+
     onTermSubmit = async term=>{
         const response = await youtube.get('/search',{
             params : {
                 q:term
             }
         })
-        this.setState({videos: response.data.item});
+        this.setState({
+            videos: response.data.items,
+            selectedVideo : response.data.items[0]
+        });
+    };
+
+    onVideoSelect = (video)=>{
+        //console.log('From the App!',video);
+        this.setState({selectedVideo : video});
     };
 
     render(){
         return <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoList videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
     }
 }
