d22dcc34538a5cb98c71c9599b71402975d9ac85
stark <anishgupta0066@gmail.com>
2021-11-11 10:19:37 +0530

    added video  thumbnails and little bit of styling

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,7 +1,15 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = props=>{
-    return <div>VideoList</div>
+const VideoItem = ({video})=>{
+    return <div className="video-item item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="thumbnail" />
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
+                </div>
+            </div>
+        </div>
 };
 
 
